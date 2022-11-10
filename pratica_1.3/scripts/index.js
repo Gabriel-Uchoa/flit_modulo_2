53859cca76dbaa923e50750a1e670f3c2697b8b7
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2022-11-09 22:12:42 -0300

    Desafio 01 da pratica 1.3 concluido

---
+++ pratica_1.3/scripts/index.js
@@ -0,0 +1,20 @@
+//Desafio 01
+
+var caminhada
+var corrida
+var horaQueAcordei = 7
+var termineiTrabalho = true
+var tenhoCompromisso
+
+if (caminhada == 5 && corrida == 2) {
+    console.log("Passo o dia Bem")
+}
+
+if (horaQueAcordei <= 8) {
+    console.log("Saio Tranquilo")
+}
+
+if (!termineiTrabalho || tenhoCompromisso) {
+    console.log('Saio Tarde')
+}
+
