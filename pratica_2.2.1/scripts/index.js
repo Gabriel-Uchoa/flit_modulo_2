35d2cb51d1c077f9a644c48573f7075d177d697e
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2022-11-17 15:31:14 -0300

    Refatoracao function handleChangeInput, parte 02 concluida

--- pratica_2.2.1/scripts/index.js
+++ pratica_2.2.1/scripts/index.js
@@ -5,41 +5,59 @@ const inputHourClosed = document.getElementById('hourClosed');
 const cliclSubmit = document.getElementById('submit');
 var checkboxes = document.querySelectorAll('[name=diasSemanaOn]');
 
+function handleChangeInput(event) {
+    window.localStorage.setItem(event.target.id, event.target.value)
+}
 
-function handleChangeName(event) {
-    console.log('Nome da Empresa: ', event.target.value)
-}
-function handleChangeAdress(event) {
-    console.log('Endereço da Empresa: ', event.target.value)
-}
-function handleChangeHourOpen(event) {
-    console.log('Hora de Abertura: ', event.target.value)
-}
-function handleChangeHourClosed(event) {
-    console.log('Hora de Fechamento: ', event.target.value)
-}
 function handleSubmit(event) {
     event.preventDefault();
     console.log("Preparando envio...")
 }
 
-
-function getValues() {
+function getValuesCheckbox() {
     var diasSemanaOn = document.querySelectorAll('[name=diasSemanaOn]:checked');
     var values = [];
+
     for (var i = 0; i < diasSemanaOn.length; i++) {
+
         values.push(diasSemanaOn[i].value);
     }
-    console.log("Dias Abertos: ", values)
+    window.localStorage.setItem('diasSemanaOn', JSON.stringify(values))
 }
 
 for (var i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener('change', getValues, false);
+    checkboxes[i].addEventListener('change', getValuesCheckbox, false);
 }
 
-inputName.addEventListener("change", handleChangeName);
-inputAdress.addEventListener("change", handleChangeAdress);
-inputHourOpen.addEventListener("change", handleChangeHourOpen);
-inputHourClosed.addEventListener("change", handleChangeHourClosed);
+function getValuesLocalStorage() {
+    if (window.localStorage.getItem("name")) {
+        inputName.value = window.localStorage.getItem("name")
+    }
+    if (window.localStorage.getItem("adress")) {
+        inputAdress.value = window.localStorage.getItem("adress")
+    }
+
+    if (window.localStorage.getItem("diasSemanaOn")) {
+        dias = JSON.parse(window.localStorage.getItem("diasSemanaOn"))
+        for (var i = 0; i < dias.length; i++) {
+            const checkbox = document.querySelector(`[value="${dias[i]}"]`);
+            checkbox.checked = true
+        }
+    }
+
+    if (window.localStorage.getItem("hourOpen")) {
+        inputHourOpen.value = window.localStorage.getItem("hourOpen")
+    }
+    if (window.localStorage.getItem("hourClosed")) {
+        inputHourClosed.value = window.localStorage.getItem("hourClosed")
+    }
+}
+
+getValuesLocalStorage()
+
+inputName.addEventListener("change", handleChangeInput);
+inputAdress.addEventListener("change", handleChangeInput);
+inputHourOpen.addEventListener("change", handleChangeInput);
+inputHourClosed.addEventListener("change", handleChangeInput);
 cliclSubmit.addEventListener("click", handleSubmit);
 
